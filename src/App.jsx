bc8919fc69c124cdf4fd4fd2ebc498e9778e9e7a
masih abedini <masih7816@gmail.com>
2024-01-25 21:02:36 +0330

    feat: install and initiliza react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import { PiSealCheckFill } from "react-icons/pi";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/student" elemen={<MainLayout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/student" elemen={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      <Toaster
+        containerStyle={{ direction: "rtl" }}
+        toastOptions={{
+          duration: 3000,
+          success: {
+            style: { color: "red !important" },
+            icon: <PiSealCheckFill className="text-green-500 text-2xl" />,
+          },
+        }}
+      />
+    </>
   );
 };
 
