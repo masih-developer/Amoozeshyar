4b26d9eb52b515e401cc880b72666fa85903023f
masih abedini <masih7816@gmail.com>
2024-01-25 19:02:23 +0330

    feat: add TextField component

---
+++ src/ui/TextField.jsx
@@ -0,0 +1,50 @@
+const TextField = ({
+  iconInput = false,
+  icon,
+  className,
+  type = "text",
+  placeholder,
+  name,
+  register,
+  validationSchema,
+  errors,
+}) => {
+  return iconInput ? (
+    <div className="flex flex-col gap-y-1">
+      <div className="relative">
+        <input
+          type={type}
+          className={`app-input--icon${className ? " " + className : ""}${
+            errors?.[name] ? " app-input--error" : ""
+          }`}
+          placeholder={placeholder}
+          {...register(name, validationSchema)}
+        />
+        {icon}
+      </div>
+      {errors?.[name] && (
+        <span className="text-xs text-right text-red-500">
+          {errors[name].message}
+        </span>
+      )}
+    </div>
+  ) : (
+    <div className="flex flex-col gap-y-1">
+      <input
+        type={type}
+        className={`app-input${className ? " " + className : ""}${
+          errors?.[name] ? " app-input--error" : ""
+        }`}
+        placeholder={placeholder}
+        {...register(name, validationSchema)}
+      />
+      {errors?.[name] && (
+        <span className="text-xs text-right text-red-500">
+          {errors[name].message}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default TextField;
