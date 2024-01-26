696a7f3fff18367e3b02fc19373fbfc35efbd761
masih abedini <masih7816@gmail.com>
2024-01-26 21:50:32 +0330

    feat: complete student status stat box

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,37 @@
 import StatBox from "../ui/StatBox";
+import { BiPrinter } from "react-icons/bi";
 
 const Home = () => {
   return (
     <div className="p-5">
       <div className="grid grid-cols-12 gap-5">
         <div className="col-span-6">
-          <StatBox title="خلاصه وضعیت تحصیلی" details></StatBox>
+          <StatBox title="خلاصه وضعیت تحصیلی" details>
+            <div className="grid grid-cols-12 w-full gap-5">
+              <div className="bg-gray-100 rounded-md p-5 col-span-4">
+                <div className="flex items-center justify-center flex-col gap-y-1">
+                  <h2 className="text-5xl font-medium">98</h2>
+                  <span className="">واحد برداشته شده</span>
+                </div>
+              </div>
+              <div className="bg-gray-100 rounded-md p-5 col-span-4">
+                <div className="flex items-center justify-center flex-col gap-y-1">
+                  <h2 className="text-5xl font-medium">87</h2>
+                  <span className="">واحد پاس شده</span>
+                </div>
+              </div>
+              <div className="bg-gray-100 rounded-md p-5 col-span-4">
+                <div className="flex items-center justify-center flex-col gap-y-1">
+                  <h2 className="text-5xl font-medium">18.5</h2>
+                  <span className="">معدل تا کنون</span>
+                </div>
+              </div>
+            </div>
+            <button className="flex items-center gap-x-2 mt-10 text-center mx-auto w-fit text-gray-500 border-b border-b-gray-500 pb-1">
+              <BiPrinter className="text-lg" />
+              چاپ کارنامه کلی دانشجو
+            </button>
+          </StatBox>
         </div>
         <div className="col-span-4">
           <StatBox
