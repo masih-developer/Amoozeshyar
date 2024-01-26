fdcb795667a2ac56807931cda749306fe5a96d5e
masih abedini <masih7816@gmail.com>
2024-01-26 21:49:21 +0330

    feat: add StatBox component

---
+++ src/ui/StatBox.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import Bullet from "./Bullet";
+
+const StatBox = ({
+  children,
+  title,
+  bulletColor = "",
+  details = false,
+  detailsLink = "",
+}) => {
+  return (
+    <div className="p-5 rounded-lg shadow-md bg-white">
+      <div className="flex items-center justify-between mb-5">
+        <div className="flex items-center gap-x-2">
+          <Bullet bulletColor={bulletColor} />
+          <h2 className="font-medium">{title}</h2>
+        </div>
+        {details && (
+          <Link
+            to={details ? detailsLink : ""}
+            className="text-gray-500 border-b border-b-gray-500"
+          >
+            جزئیات
+          </Link>
+        )}
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default StatBox;
