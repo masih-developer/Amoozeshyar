f024d424063f8e50db3a18e71634380607f5e3ee
masih abedini <masih7816@gmail.com>
2024-01-27 18:13:14 +0330

    fix: resolve app width

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,14 +4,12 @@ import Topbar from "../ui/Topbar";
 
 const MainLayout = () => {
   return (
-    <div className="container">
-      <div className="flex">
-        <Sidebar />
-        <main className="w-[calc(100%-250px)] mr-auto bg-gray-100">
-          <Topbar />
-          <Outlet />
-        </main>
-      </div>
+    <div className="flex">
+      <Sidebar />
+      <main className="w-[calc(100%-250px)] mr-auto bg-gray-100 min-h-screen">
+        <Topbar />
+        <Outlet />
+      </main>
     </div>
   );
 };
