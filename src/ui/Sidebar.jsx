520613b246fd9b19bf5fb8afc269a72d5ee6da22
masih abedini <masih7816@gmail.com>
2024-01-27 18:11:03 +0330

    fix: fix Sidebar height

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,7 +2,7 @@ import SidebarContent from "./SidebarContent";
 
 const Sidebar = () => {
   return (
-    <div className="w-[250px] h-screen overflow-y-auto bg-primary-darker text-white p-3">
+    <div className="w-[250px] overflow-y-auto bg-primary-darker text-white p-3 fixed right-0 top-0 bottom-0">
       <SidebarContent />
     </div>
   );
